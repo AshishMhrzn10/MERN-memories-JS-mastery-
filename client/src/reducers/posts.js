e6987c66cf87dc9a7e9f78cca5aac7c447ccf2ec
Ashish Maharjan <amaharjan1032@gmail.com>
2021-03-18 10:27:54 +0545

    Part1 done

--- client/src/reducers/posts.js
+++ client/src/reducers/posts.js
@@ -3,8 +3,8 @@ export default (posts = [], action) => {
         case 'FETCH_ALL':
             return action.payload;
 
-        case 'FETCH_ALL':
-            return posts;
+        case 'CREATE':
+            return [...posts, action.payload];
 
         default:
             return posts;
